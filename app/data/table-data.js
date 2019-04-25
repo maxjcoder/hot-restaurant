c6807a997a7f982c90719f213993f53f3b89ed24
team awesome <maxj@email.arizona.edu>
2019-04-25 16:25:43 -0700

    files-folders, html, pkgs, server.js updates

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+        customerName: 'Ahmed',
+        customerEmail: 'ahmed@example.com',
+        customerID: 'afhaque90',
+        phoneNumber: '000-000-0000'
+    }
+];
+
+module.exports = tableArray;
